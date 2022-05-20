bf13d530ed7631d6ea7099f23cfbbb5372c64f5f
mark terry <mark@neptune.rootmeansquare.com>
2022-05-20 16:11:22 -0600

    glider dirs

---
+++ APPS/Glider/MENUS/CTP.m
@@ -0,0 +1,6 @@
+title ChooseViaMorK
+type CHOICE
+item ReplaceViaName M_VALUE K
+help  replace via input of name
+item ReplaceViaMouse M_VALUE M
+help  replace via mouse pick
---
+++ APPS/Glider/MENUS/NTP.m
@@ -0,0 +1,7 @@
+title TP-Choice
+type CHOICE
+item Replace M_VALUE R
+item Delete M_VALUE D
+item Insert M_VALUE I
+item Add M_VALUE A
+help  change TP
